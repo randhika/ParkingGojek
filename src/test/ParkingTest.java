70ef345eb7573273923d9d8e19f691964a7e88c0
Randhika Ryan <randhika.ryan@kompas.com>
2016-08-09 10:59:17 +0700

    remove test

--- src/test/ParkingTest.java
+++ src/test/ParkingTest.java
@@ -1,17 +1,6 @@
 package test;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
-import parking.Parking;
 
 public class ParkingTest {
-	@Test
-	public void TestisParkingCreated() throws Exception {
-
-		Parking createParkingTester = new Parking();
-		assertEquals(false, createParkingTester.isParkingLotCreated());
-		createParkingTester.createParkingLot(6);
-		assertEquals(true, createParkingTester.isParkingLotCreated());
-	}
 
 }
